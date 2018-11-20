ed0fac9133fad4f92426fa9f103555eaae9dddaf
Paul Lucas <plucas@salesforce.com>
2018-11-20 14:37:38 +1100

    Set the home phone number

--- server.js
+++ server.js
@@ -14,20 +14,21 @@ app.post('/update', function(req, res) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
+            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1, HomePhone = $1  WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
             [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
             function(err, result) {
+                const phoneNumber = req.body.phone.trim();
+
                 if (err != null || result.rowCount == 0) {
-                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
-                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, HomePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5, $6)',
+                  [phoneNumber, phoneNumber, phoneNumber, req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
                   function(err, result) {
                     done();
                     if (err) {
                         res.status(400).json({error: err.message});
                     }
                     else {
-                        // this will still cause jquery to display 'Record updated!'
-                        // eventhough it was inserted
+                        // this will still cause jquery to display 'Record updated!' even though it was inserted.
                         res.json(result);
                     }
                   });
